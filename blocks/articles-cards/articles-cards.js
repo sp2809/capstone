4bb20ec6c72b435aebb4287fffba272ccc6d57a8
sp2809 <pandeysaumya533@gmail.com>
2024-09-27 09:26:52 +0530

    articles-cards block added

---
+++ blocks/articles-cards/articles-cards.js
@@ -0,0 +1,38 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+async function fetchCardsData(offset, limit) {
+    const response = await fetch(`/query-index.json?offset=${offset}&limit=${limit}`);
+    const data = await response.json();
+    return data;
+  }
+
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('.cards-card-body .button-container a').forEach((a, index) => {
+    const picture = ul.querySelectorAll('picture')[index];
+    if (picture) {
+      const link = a.cloneNode(true);
+      link.textContent = '';
+      picture.parentNode.insertBefore(link, picture);
+      link.appendChild(picture);
+    }
+  });
+  ul.querySelectorAll('.cards-card-body').forEach((container) => {
+    const paragraphs = container.querySelectorAll('p');
+    if (paragraphs.length >= 2) {
+      paragraphs[1].classList.add('card-paragraph'); // Adds 'new-class' to the second <p>
+    }
+  });
+  block.textContent = '';
+  block.append(ul);
+}
